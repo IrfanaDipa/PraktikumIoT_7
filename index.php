77613398ef40def14775c58438bdbe620ac72c61
IrfanaDipa <irfanadiprasetya@gmail.com>
2025-09-30 21:54:33 +0700

    Add Index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+  <title>LED ON/OFF</title>
+  </head>
+
+  <body>
+    <p align="center">
+      <font size="8">
+        <b>
+          LED 1 = &nbsp
+          <a href="https://api.thingspeak.com/update?api_key=8PJBRCJGV5DU4MJM&field1=1">ON</a>
+        </b>
+        /
+        <b>
+          <a href="https://api.thingspeak.com/update?api_key=8PJBRCJGV5DU4MJM&field1=0">OFF</a>
+        </b>
+        <br>
+        <b>
+          LED 2 = &nbsp
+          <a href="https://api.thingspeak.com/update?api_key=8PJBRCJGV5DU4MJM&field2=1">ON</a>
+        </b>
+        /
+        <b>
+          <a href="https://api.thingspeak.com/update?api_key=8PJBRCJGV5DU4MJM&field2=0">OFF</a>
+        </b>
+        <br>
+      </font>
+    </p>
+  </body>
+</html>
